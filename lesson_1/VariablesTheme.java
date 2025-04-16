76b1cac63c2e925c7aab04c32e390aa94306b668
Maksim Glazunov <magus.mgl@gmail.com>
2025-04-16 22:39:00 +0500

    hm_1 part 2 fix reivew comments 4

--- lesson_1/VariablesTheme.java
+++ lesson_1/VariablesTheme.java
@@ -33,43 +33,35 @@ public class VariablesTheme {
         System.out.println("\tСтоимость товаров со скидкой: " + discountPriceBd);
 
         System.out.println("\n2.ВЫВОД ASCII-ГРАФИКИ");
-        char slash = '/';
-        char backSlash = '\\';
-        char leftParenthesis = '(';
-        char rightParenthesis = ')';
-        char underscore = '_';
-        
-        System.out.println("                               " + slash + backSlash); 
-        System.out.println("\t    J    a  v     v   " + slash + "  " + backSlash);
-        System.out.println("\t    J   a a  v   v   " + slash + underscore + leftParenthesis +
-                " " + rightParenthesis + backSlash);
-        System.out.println("\t J  J  aaaaa  V V   " + slash + "      " + backSlash);
-        System.out.println("\t  JJ  a     a  V   " + slash + underscore + underscore + underscore +
-                        underscore + slash + backSlash + underscore + underscore + backSlash);
+        System.out.println("                               /\\"); 
+        System.out.println("\t    J    a  v     v   /  \\");
+        System.out.println("\t    J   a a  v   v   /_( )\\");
+        System.out.println("\t J  J  aaaaa  V V   /      \\");
+        System.out.println("\t  JJ  a     a  V   /____/\\__\\");
 
         System.out.println("\n3. ТЕСТИРОВАНИЕ ДАТЧИКОВ ПЕРЕД ЗАПУСКОМ РАКЕТЫ");
-        byte temperature = Byte.MAX_VALUE;
-        System.out.println("\t[Температура, °C]"); 
-        System.out.println("\tИсходная: " + temperature);
-        System.out.println("\t+1: " + ++temperature);
-        System.out.println("\t-1: " + --temperature); 
+        byte temp = Byte.MAX_VALUE;
+        System.out.println("\n\t[Температура, °C]"); 
+        System.out.println("\tИсходная: " + temp);
+        System.out.println("\t+1: " + ++temp);
+        System.out.println("\t-1: " + --temp); 
         short pressure = Short.MAX_VALUE;
-        System.out.println("\t[Давление, кг/см2]"); 
+        System.out.println("\n\t[Давление, кг/см2]"); 
         System.out.println("\tИсходное: " + pressure);
         System.out.println("\t+1: " + ++pressure);
         System.out.println("\t-1: " + --pressure);
         char systemStatusCode = Character.MAX_VALUE;
-        System.out.println("\t[Код состояния системы]"); 
+        System.out.println("\n\t[Код состояния системы]"); 
         System.out.println("\tИсходное: " + (int) systemStatusCode);
         System.out.println("\t+1: " + (int) ++systemStatusCode);
         System.out.println("\t-1: " + (int) --systemStatusCode);
         int traveledDistance = Integer.MAX_VALUE;
-        System.out.println("\t[Пройденная дистанция, км]"); 
+        System.out.println("\n\t[Пройденная дистанция, км]"); 
         System.out.println("\tИсходная: " + traveledDistance);
         System.out.println("\t+1: " + ++traveledDistance);
         System.out.println("\t-1: " + --traveledDistance);
         long sinceStartTime = Long.MAX_VALUE;
-        System.out.println("\t[Время с момента старта, с]"); 
+        System.out.println("\n\t[Время с момента старта, с]"); 
         System.out.println("\tИсходное: " + sinceStartTime);
         System.out.println("\t+1: " + ++sinceStartTime);
         System.out.println("\t-1: " + --sinceStartTime);
@@ -102,32 +94,27 @@ public class VariablesTheme {
         int fourthCode = 1074;
         int fifthCode = 1077;
         int sixthCode = 1090;
-        System.out.printf("%6d%6d%6d%6d%6d%6d%n", firstCode, secondCode, firdCode,
-                        fourthCode, fifthCode, sixthCode);
-
-        String firstSymbol = String.valueOf((char) firstCode);
-        String secondSymbol = String.valueOf((char) secondCode);
-        String firdSymbol = String.valueOf((char) firdCode);
-        String fourthSymbol = String.valueOf((char) fourthCode);
-        String fifthSymbol = String.valueOf((char) fifthCode);
-        String sixthSymbol = String.valueOf((char) sixthCode);
-        System.out.printf("%6s%6s%6s%6s%6s%6s%n", firstSymbol, secondSymbol,
-                        firdSymbol, fourthSymbol, fifthSymbol, sixthSymbol);
+        System.out.printf("%6d%6d%6d%6d%6d%6d%n", firstCode, secondCode,
+                firdCode, fourthCode,
+                fifthCode, sixthCode);
+        System.out.printf("%6c%6c%6c%6c%6c%6c%n", firstCode, secondCode,
+                firdCode, fourthCode,
+                fifthCode, sixthCode);
         
-        System.out.println("\n6. МАНИПУЛЯЦИИ С СОТНЯМИ, ДЕСЯТКАМИ И ЕДИНИЦАМИ ЧИСЛА");
-        int number = 123;
-        int ones = number % 10;
-        int tens = (number / 10) % 10;
-        int hundreds = (number / 10) / 10;
-        int digitsSum = ones + tens + hundreds;
-        int digitsProduct = ones * tens * hundreds;
+        System.out.println("\n6. АНАЛИЗ КОДА ТОВАРА");
+        int productCode = 123;
+        int typePackaging = productCode % 10;
+        int productSubcategory = (productCode / 10) % 10;
+        int productCategory = (productCode / 10) / 10;
+        int checkSum = typePackaging + productSubcategory + productCategory;
+        int verifCode = typePackaging * productSubcategory * productCategory;
 
-        System.out.println("\tЧисло " + number + " содержит:");
-        System.out.println("\t\tсотен - " + hundreds);
-        System.out.println("\t\tдесятков - " + tens);
-        System.out.println("\t\tединиц - " + ones);
-        System.out.println("\t\tСумма разрядов = " + digitsSum); 
-        System.out.println("\t\tПроизведение разрядов = " + digitsProduct); 
+        System.out.println("\tКод товара: " + productCode);
+        System.out.println("\t\tкатегория товара - " + productCategory);
+        System.out.println("\t\tподкатегория - " + productSubcategory);
+        System.out.println("\t\tтип упаковки - " + typePackaging);
+        System.out.println("\tКонтрольная сумма = " + checkSum); 
+        System.out.println("\tПроизведение разрядов = " + verifCode); 
                 
         System.out.println("\n7 ВЫВОД ПАРАМЕТРОВ JVM И ОС");
         Runtime runtime = Runtime.getRuntime();
@@ -136,7 +123,7 @@ public class VariablesTheme {
         long freeMemory = runtime.freeMemory() / (1024 * 1024);
         final long totalMemory = runtime.maxMemory() / (1024 * 1024);
         System.out.println("\tДоступное количество ядер: " + availableCors);
-        System.out.println("\tИспользуемый объем памяти (Мб): " + usedMemory);;
+        System.out.println("\tИспользуемый объем памяти (Мб): " + usedMemory);
         System.out.println("\tСвободный объем памяти (Мб): " + freeMemory);
         System.out.println("\tМаксимально доступный объем памяти (Мб): " + totalMemory);
         System.out.println("\tСистемный диск: " + System.getProperty("user.home"));
@@ -146,13 +133,13 @@ public class VariablesTheme {
                 System.getProperty("file.separator"));
 
         System.out.println("\n8. ЗАМЕР ВРЕМЕНИ РАБОТЫ КОДА"); 
-        LocalTime endLocalTime = LocalTime.now();        
+        LocalTime endLocalTime = LocalTime.now();
         long endTime = System.nanoTime();
         double executionTime = (endTime - startTime) / 1_000_000_000.0;
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
     
         System.out.printf("\t| %-18s | %-12s | %n", "Начало проверки", dtf.format(startLocalTime));
-        System.out.println("\t+-----------------------------------+"); 
+        System.out.println("\t+-----------------------------------+");
         System.out.printf("\t| %-18s | %-12s | %n", "Окончание проверки", dtf.format(endLocalTime));
         System.out.println("\t+-----------------------------------+"); 
         System.out.printf("\t| %-18s | %5.3f секунд | %n", "Время работы", executionTime);
