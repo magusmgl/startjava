1197fc12b2a54aee91191dd3f83fc87b0df6355f
Maksim Glazunov <magus.mgl@gmail.com>
2025-04-28 22:41:32 +0500

    hm_2 person class

---
+++ lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    String gender = "male";
+    String name = "Robert";
+    int height = 187;
+    int weight = 88;
+    int age = 35;
+
+    void move() {
+        System.out.println("Moving");
+    }
+
+    void sit() {
+        System.out.println("Sitting");
+    }
+
+    void run() {
+        System.out.println("Runing");
+    }
+
+    void speak() {
+        System.out.println("Speaking");
+    }
+
+    void learnJava() {
+        System.out.println("Learn Java");
+    }
+}
